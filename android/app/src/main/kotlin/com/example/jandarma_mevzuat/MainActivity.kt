53b831a20bd53c547db0c0dfd9272a9178388e15
Ercan Garip <ercngp@gmail.com>
2021-02-17 22:54:01 +0300

    first

---
+++ android/app/src/main/kotlin/com/example/jandarma_mevzuat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jandarma_mevzuat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:jandarma_mevzuat/screens/home/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage()));
+}
